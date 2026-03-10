851b35ec43bcc4cefac6698e92b103d38c1218f4
wittns <mugishawitness15@gmail.com>
2026-03-10 13:54:41 +0200

    updationg security middlewares

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express"
 import errorHandler from './middleware/errorhandler.js'
 import dotenv from "dotenv"
+import helmet from "helmet";
+import hpp from "hpp";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import xss from "xss-clean";
+import compression from "compression"
 import router from "./routes/taskRoute.js"
 import cookieParser from 'cookie-parser'
 import mongoSanitize from 'express-mongo-sanitize'
@@ -8,8 +14,13 @@ dotenv.config()
 import connectDB from "./config/db.js"
 const app = express();
 app.use(express.json());
-app.use(mongoSanitize())
-app.use(cookieParser())
+app.use(helmet());
+app.use(cors())
+app.use(mongoSanitize());
+app.use(compression());
+app.use(xss());
+app.use(hpp());
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.use("/",router);
