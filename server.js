9e4ccd82bf90e94d9c913709e6ca112e70f10df3
wittns <mugishawitness15@gmail.com>
2026-03-13 08:18:12 +0200

    updating middlewares

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ import hpp from "hpp";
 import cors from "cors";
 import compression from "compression"
 import router from "./routes/taskRoute.js"
+import ApiError from "./middleware/class.js";
 import cookieParser from 'cookie-parser'
-dotenv.config()
 import connectDB from "./config/db.js"
 const app = express();
+
+dotenv.config()
+connectDB()
+app.use(compression());
+
+app.use(helmet());
+
 const allowedOrigins = [
   "http://localhost:3000",
   "https://maxus.com"
@@ -29,8 +36,9 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200
 }
-app.use(helmet());
+
 app.use(cors(corsOptions))
+app.use(limiter)
 
 
 app.use(express.json());
@@ -39,15 +47,13 @@ app.use(cookieParser());
 
 app.use(hpp());
 
-app.use(limiter)
-
 app.use("/",router);
-
-app.use(compression());
-
+app.all("*", (req,res,next) => {
+  next( new ApiError(`Can not find ${req.originalUrl} on this server`, 404))
+})
 app.use(errorHandler)
 const PORT = process.env.PORT || 5000;
-connectDB();
+
 
 app.listen(PORT,()=> {
   console.log(`server running on port ${PORT} `)  
